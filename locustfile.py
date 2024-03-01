cfdd7a4b316610016803c4868e66536e57977155
balusu-bhanu-prakash <balusubhanuprakash091@gmail.com>
2024-03-01 16:15:49 +0530

    e5t6

--- locustfile.py
+++ locustfile.py
@@ -6,7 +6,3 @@ class WebsiteUser(HttpUser):
     @task
     def test_hello_world(self):
         self.client.get("/")  # Simulates a GET request to the homepage route
-
-if __name__ == "__main__":
-    import os
-    os.system(f"locust -f locustfile.py --headless --host={os.getenv('HOST')} --users {os.getenv('USERS')} --spawn-rate {os.getenv('HATCH_RATE')}")
